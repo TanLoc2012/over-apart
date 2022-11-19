93313b23dcb2de9d9cbc089f24b860b4f78def46
AoNoAdmiral <75779513+nhatlamairforce@users.noreply.github.com>
2022-11-20 03:48:56 +0700

    X
    A

--- src/components/Shopinfo/HeaderPSection.js
+++ src/components/Shopinfo/HeaderPSection.js
@@ -1,14 +1,45 @@
 import './HeaderPSection.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-function Shop() {
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
+function ShopInfo({ product }) {
+    console.log(product.img.naturalHeight);
     return (
-        <div className="container container__ìno">
-        
-                <p>HELLO</p>
-
+        <div className="container container__info">
+            <div className="first_box">
+                <div className="left_column">
+                    <img src={product.img} alt="Product image" className="select"></img>
+                    <Swiper className="swiper" slidesPerView={4}>
+                        <div className="swiper-wrapper">
+                        <SwiperSlide className="swiper-slide">
+                            <img src={product.img} alt="Product image" className="option"></img>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <img src={product.img} alt="Product image" className="option"></img>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <img src={product.img} alt="Product image" className="option"></img>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <img src={product.img} alt="Product image" className="option"></img>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <img src={product.img} alt="Product image" className="option"></img>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <img src={product.img} alt="Product image" className="option"></img>
+                        </SwiperSlide>
+                        </div>
+                    </Swiper>
+                </div>
+                <div className="right_column">
+                    <h1> {product.productName}</h1>
+                    <p className="code"> {product.code}</p>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default Shop;
+export default ShopInfo;

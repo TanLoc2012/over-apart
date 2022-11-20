6e5f2a20f43957663c1fb455c8a78bf9b6f78416
AoNoAdmiral <75779513+nhatlamairforce@users.noreply.github.com>
2022-11-20 22:08:21 +0700

    X
    A

--- src/pages/customer/InfoProduct.js
+++ src/pages/customer/InfoProduct.js
@@ -24,7 +24,7 @@ function InfoProductPage() {
         <div>
             <Header></Header>
             <Breadcrumb child={child}></Breadcrumb>
-            <HeaderPSection product={{ img: images.f1,productName: 'Bộ bàn ghế Barcelona 1',rating: 5,sold: 20,price: '6.900.000 đ',code:'MÃ SẢN PHẨM 2001353'}}></HeaderPSection>
+            <HeaderPSection product={{ img: images.f1,productName: 'Bộ bàn ghế Barcelona 1',rating: 5,sold: 20,like:5,comment:5,price: '6.900.000 đ',code:'MÃ SẢN PHẨM 2001353'}}></HeaderPSection>
             <Footer></Footer>
         </div>
     );
